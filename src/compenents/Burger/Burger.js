70f39fad124ace08e6c105afda605a373490eab1
MitchGaines <gainesmitch2@gmail.com>
2020-04-10 21:26:11 -0400

    add/remove items from burger, removal unsafe

--- src/compenents/Burger/Burger.js
+++ src/compenents/Burger/Burger.js
@@ -4,13 +4,23 @@ import classes from './Burger.module.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+    let transformed_ingredients = Object.keys(props.ingredients)
+        .map(ing_key => {
+            return [...Array(props.ingredients[ing_key])].map((_, i) => {
+                return <BurgerIngredient key={ing_key + i} type={ing_key}/>;
+            });
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformed_ingredients.length === 0) {
+        transformed_ingredients = <p>Fill me up!</p>;
+    }
     return (
         <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="bacon"/>
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="bread-bottom" />
+            <BurgerIngredient type="bread-top"/>
+            {transformed_ingredients}
+            <BurgerIngredient type="bread-bottom"/>
         </div>
     );
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Aux from '../../hoc/Aux';
 import Burger from '../../compenents/Burger/Burger';
+import BuildControls from '../../compenents/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.25,
+    meat: 2,
+    bacon: 0.75
+};
 
 function BurgerBuilder() {
+    const [ingredients, updateIngredients] = useState({salad: 0, bacon: 0, cheese: 0, meat: 0});
+    const [price, updatePrice] = useState(0.00);
+
+    const addIngredientHandler = (type) => {
+        const new_count = ingredients[type] + 1;
+        let updated_ingredients = ingredients;
+        updated_ingredients[type] = new_count;
+        updateIngredients(updated_ingredients);
+        const add_price = INGREDIENT_PRICES[type];
+        updatePrice(price + add_price);
+    };
+
+    const removeIngredientHandler = (type) => {
+        const new_count = ingredients[type] - 1;
+        let updated_ingredients = ingredients;
+        updated_ingredients[type] = new_count;
+        updateIngredients(updated_ingredients);
+        const add_price = INGREDIENT_PRICES[type];
+        updatePrice(price - add_price);
+    };
+
     return (
         <Aux>
-            <Burger/>
-            <div>Build Controls</div>
+            <Burger ingredients={ingredients}/>
+            <BuildControls ingredientAdded={addIngredientHandler} ingredientRemoved={removeIngredientHandler} />
         </Aux>
     );
 }
